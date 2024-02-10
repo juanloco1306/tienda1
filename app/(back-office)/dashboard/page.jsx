9002e9b6d278e869cce96a0f6b23527eb3acaf5b
unknown <juanloco1306@gmail.com>
2024-02-09 21:16:50 -0600

    segundo commit

--- app/(back-office)/dashboard/page.jsx
+++ app/(back-office)/dashboard/page.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Heading from "../../../components/backoffice/Heading"
 import LargeCards from '../../../components/backoffice/LargeCards'
+import SmallCards from '@/components/backoffice/SmallCards'
 
 export default function page() {
   return (
     <div>
         <Heading title="Dashboard Overview"/>
         <LargeCards/>
+        <SmallCards/>
     </div>
   )
 }
