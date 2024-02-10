5af16206c3a6bba724d2f15a3c2d3333a4cd47a8
juanloco1306 <96637911+juanloco1306@users.noreply.github.com>
2024-02-10 12:35:24 -0600

    Update page

--- app/(back-office)/dashboard/page.jsx
+++ app/(back-office)/dashboard/page.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import Heading from "../../../components/backoffice/Heading"
-import LargeCards from '../../../components/backoffice/LargeCards'
-import SmallCards from '@/components/backoffice/SmallCards'
+import DashboardCharts from "../../../components/backoffice/DashboardCharts";
+import Heading from "../../../components/backoffice/Heading";
+import LargeCards from '../../../components/backoffice/LargeCards';
+import SmallCards from '@/components/backoffice/SmallCards';
 
 export default function page() {
   return (
@@ -9,6 +10,7 @@ export default function page() {
         <Heading title="Dashboard Overview"/>
         <LargeCards/>
         <SmallCards/>
+        <DashboardCharts/>
     </div>
   )
 }
