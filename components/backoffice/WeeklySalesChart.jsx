50cdb933b1646ea9a5a55a764acde9358cb60870
juanloco1306 <96637911+juanloco1306@users.noreply.github.com>
2024-02-10 13:18:28 -0600

    Update WeeklySalesChart 2

--- components/backoffice/WeeklySalesChart.jsx
+++ components/backoffice/WeeklySalesChart.jsx
@@ -2,8 +2,22 @@ import React from "react";
 
 export default function WeeklySalesChart(){
   return(
-    <div>
-      <h2>Weekly Charts</h2>
+    <div className="bg-slate-700 p-8 rounded-lg">
+      <h2 className="text-xl font-bold mb-4">Weekly Charts</h2>
+    <div className="p-4">
+      
+
+<div className="text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:text-gray-400 dark:border-gray-700">
+    <ul className="flex flex-wrap -mb-px">
+        <li className="me-2">
+            <button className="inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300 dark:hover:text-gray-300">Sales</button>
+        </li>
+        <li className="me-2">
+            <button className="inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active dark:text-blue-500 dark:border-blue-500" aria-current="page">Orders</button>
+        </li>
+    </ul>
+</div>
+    </div>
     </div>
   );
 }
