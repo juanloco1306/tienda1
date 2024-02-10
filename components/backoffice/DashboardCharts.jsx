fb363e5e52b10d62cec23455a24b26ee72e3235f
juanloco1306 <96637911+juanloco1306@users.noreply.github.com>
2024-02-10 12:33:10 -0600

    Update DashboardCharts

--- components/backoffice/DashboardCharts.jsx
+++ components/backoffice/DashboardCharts.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import WeeklySalesChart from "./WeeklySalesChart";
+import BestSellingProductsChart from "./BestSellingProductsChart";
 
 export default function DashboardCharts(){
-  return <div className="grid grid-cols-1 lg:grid-cols-2">
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2">
+    <WeeklySalesChart/>
+    <BestSellingProductsChart/>
   </div>
+ );
 }
