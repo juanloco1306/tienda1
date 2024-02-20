a11525a662ed3e07c8906893fca17908c8645441
Juan Jose Jimenez <juanjosejimenez@MacBook-Pro-de-Juan.local>
2024-02-19 23:51:32 -0600

    termine video 5

--- app/(back-office)/dashboard/(catalogue)/categories/new/page.jsx
+++ app/(back-office)/dashboard/(catalogue)/categories/new/page.jsx
@@ -1,13 +1,18 @@
 "use client"
 import SubmitButton from '@/components/FormInputs/SubmitButton'
+import TextareaInput from '@/components/FormInputs/TextAreaInput'
 import TextInput from '@/components/FormInputs/TextInput'
 import FormHeader from '@/components/backoffice/FormHeader'
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import TextareaInput from '@/components/FormInputs/TextAreaInput'
+import { generateSlug } from '@/lib/generateSlug'
 
 export default function NewCategory() {
   const {register,handleSubmit,formState:{errors}} = useForm();
   async function onSubmit(data){
+    const slug = generateSlug(data.title)
+    data.slug=slug
     console.log(data);
   }
   return (
@@ -16,6 +21,7 @@ export default function NewCategory() {
       <form onSubmit={handleSubmit(onSubmit)} className='w-full max-w-4xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 mx-auto my-3'>
       <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
         <TextInput label="Category Title" name="title" register={register} errors={errors}/>
+        <TextareaInput label="Category Description" name="description" register={register} errors={errors}/>
       </div>
       <SubmitButton isLoading={false} buttonTitle="Create Category" LoadingButtonTitle="Creating Category please wait..."/>
       </form>
