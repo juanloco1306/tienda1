c9f44a55906ff78f15ad5b019135116cdeb5739c
unknown <juanloco1306@gmail.com>
2024-02-20 21:41:41 -0600

    Me quede en una hora con cincuenta minutos

--- app/(back-office)/dashboard/(catalogue)/coupons/new/page.jsx
+++ app/(back-office)/dashboard/(catalogue)/coupons/new/page.jsx
@@ -9,14 +9,12 @@ import { generateSlug } from '@/lib/generateSlug'
 import ImageInput from '@/components/FormInputs/ImageInput'
 import { makePostRequest } from '@/lib/apiRequest'
 
-export default function NewCategory() {
-  const [imageUrl,setImageUrl] = useState("")
+export default function NewCoupon() {
   const [loading,setLoading] = useState(false)
   const {register,reset,handleSubmit,formState:{errors}} = useForm();
   async function onSubmit(data){
     const slug = generateSlug(data.title)
     data.slug=slug
-    data.imageUrl=imageUrl
     console.log(data);
     makePostRequest(
       setLoading,
@@ -29,14 +27,14 @@ export default function NewCategory() {
   }
   return (
     <div>
-      <FormHeader title="New category"/>
+      <FormHeader title="New Coupon"/>
       <form onSubmit={handleSubmit(onSubmit)} className='w-full max-w-4xl p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 mx-auto my-3'>
       <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
-        <TextInput label="Category Title" name="title" register={register} errors={errors}/>
-        <TextareaInput label="Category Description" name="description" register={register} errors={errors}/>
-        <ImageInput imageUrl={imageUrl} setImageUrl={setImageUrl} endpoint="categoryImageUploader" label="Category Image"/>
+        <TextInput label="Coupon Title" name="title" register={register} errors={errors}/>
+        <TextInput label="Coupon Code" name="couponCode" register={register} errors={errors} className='w-full'/>
+        <TextInput label="Coupon Expiry Date" name="expiryDate" type='date' register={register} errors={errors} className='w-full' />
       </div>
-      <SubmitButton isLoading={loading} buttonTitle="Create Category" LoadingButtonTitle="Creating Category please wait..."/>
+      <SubmitButton isLoading={loading} buttonTitle="Create Coupon" LoadingButtonTitle="Creating Coupon please wait..."/>
       </form>
     </div>
   )
--- app/(back-office)/dashboard/(catalogue)/coupons/page.jsx
+++ app/(back-office)/dashboard/(catalogue)/coupons/page.jsx
@@ -1,9 +1,18 @@
+import Heading from '@/components/backoffice/Heading'
 import React from 'react'
+import Link from 'next/link'
+import PageHeader from '@/components/backoffice/PageHeader'
+import TableActions from '@/components/backoffice/TableActions'
 
-export default function page() {
+export default function Coupons() {
   return (
     <div>
-        <h2>Coupons</h2>
+      <PageHeader heading="Coupons" href="/dashboard/coupons/new" LinkTitle="Add Coupon"/>
+      <TableActions/>
+
+      <div className="py-8">
+        <h2>Table</h2>
+      </div>
     </div>
   )
 }
